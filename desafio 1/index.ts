b31b6739eb726a029020fc132bc85ac45022c9ca
ciane-pereira <ciciap@terra.com.br>
2022-07-30 19:29:22 -0300

    Desafio typeScript

---
+++ desafio 1/index.ts
@@ -0,0 +1,16 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+/*let employee = {};
+employee.code = 10;
+employee.name = "John";*/ 
+
+let funcionario1 = {
+    nome: 'Maria',
+    codigo: 11,
+};
+
+let funcionario2 = {
+    nome: 'Jhon',
+    codigo: 10,
+};
+
---
+++ desafio 2/index.ts
@@ -0,0 +1,25 @@
+// Como podemos melhorar esse código usando TS? 
+
+let pessoa1 = {
+    nome: 'Maria',
+    idade: 29,
+    profissao: 'atriz'
+}; 
+
+let pessoa2 = {
+    nome: 'Roberto',
+    idade: 19,
+    profissao: 'padeiro'
+};
+
+let pessoa3 = {
+    nome: 'Laura',
+    idade: 32,
+    profissao: 'atriz'
+};
+
+let pessoa4 = {
+    nome: 'Carlos',
+    idade: 19,
+    profissao: 'padeiro'
+}
